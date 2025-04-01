49293355ef02e570cf8fc6f135ddf4251a319df4
Velislav Velikov <50297857+Webeltek@users.noreply.github.com>
2025-04-01 21:28:14 +0200

    created date string in home view converted to locale string

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import './Home.css'
 import Loader from "../shared/Loader";
 import { useLatestItems } from "../../api/itemApi";
+import { fromIsoDateString } from "../../utils/fromIsoDateString.js";
 
 const imagesUrl = import.meta.env.VITE_IMAGES_URL;
 
@@ -40,7 +41,7 @@ export default function Home(){
                                 )}
                                 
                                 <img src={item.image} />
-                                <p>Added: {item.created_at }</p>
+                                <p>Added: {fromIsoDateString(item.created_at) }</p>
                                 <h3> {item.model}</h3>
                                 <p>${item.price}</p>
                                 <Link to={`/items/${item._id}/details`} className="btn">View Details</Link>
