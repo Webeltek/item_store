9b83f9852db5a1cf29f381cc1aa685059f4e0034
Velislav Velikov <50297857+Webeltek@users.noreply.github.com>
2025-04-20 23:19:13 +0200

    Add firebase hosting for the client

--- client/src/components/edit-item/EditItem.jsx
+++ client/src/components/edit-item/EditItem.jsx
@@ -51,7 +51,6 @@ export default function EditItem() {
     }
     
 
-
     return (
         <>
         <section className="edit-hero">
--- server/config/config.js
+++ server/config/config.js
@@ -1,15 +1,25 @@
 const env = process.env.NODE_ENV || 'development';
-
+//included CORS origins for running locally with remote backend
+// when running frontend with npm run preview it runs on none default port 4173    
 const config = {
     development: {
         port: process.env.PORT || 3100,
         dbURL: 'mongodb://127.0.0.1:27017/itemstore',
-        origin: ['http://localhost:5173','http://localhost:5174']
+        origin: [
+            'http://localhost:4173',
+            'http://localhost:4174',
+            'http://localhost:5173'
+        ]
     },
     production: {
         port: process.env.PORT || 3100,
         dbURL: process.env.DB_URL_CREDENTIALS,
-        origin: ['https://item-store-wlyi.onrender.com']
+        origin: [
+            process.env.ORIGIN_RENDER,
+            process.env.ORIGIN_FIREBASE,
+            'http://localhost:4173', 
+            'http://localhost:5173',
+            'http://localhost:4173']
     }
 };
 
